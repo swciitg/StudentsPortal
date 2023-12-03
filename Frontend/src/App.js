d42f01269d767908d98686ecd72a9857fc86e40e
kvineet002 <134228421+kvineet002@users.noreply.github.com>
2023-12-03 23:06:09 +0530

    made--profileselection--home

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter,Route,Routes } from "react-router-dom";
-import HomePage from './Pages/HomePage';
 import ProfileSelection from './Pages/ProfileSelection';
 import StudentsSignUp from './Pages/Student/StudentsSignUp';
 import Otp from './Pages/Student/otp';
@@ -35,8 +34,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/ProfileSelection" element={<ProfileSelection/>}/>
+        <Route path="/" element={<ProfileSelection/>}/>
         <Route path="/StudentsSignUp" element={<StudentsSignUp/>}/>
         <Route path="/Otp" element={<Otp/>}/>
         <Route path="/CreatePass" element={<CreatePass/>}/>
