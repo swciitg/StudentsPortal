1a623af007cc5e1b68a4f516a8f06d480560b59f
Durgesh Kumar Deo <durgeshdeo1410@gmail.com>
2024-01-07 05:12:12 +0530

    forgot password ui

--- Backend/middleware/updateToken.js
+++ Backend/middleware/updateToken.js
@@ -1,28 +1,25 @@
 import { Admins } from "../Models/Admins.js";
-const middleware ={};
+const middleware = {};
 
- middleware.checkAdminToken = async function(req,res,next){
-    const email=req.body.email;
-    const Admin = await Admins.findOne({email});
-    if(Admin){
-        if(Admin.token==req.body.token){
-            return next();
-
-        }
-        else{
-            return await res.status(401).json({
-                status:"error",
-                data:[],
-                msg:"Unauthorised"
-            });
-        }
+middleware.checkAdminToken = async function (req, res, next) {
+  const email = req.body.email;
+  const Admin = await Admins.findOne({ email });
+  if (Admin) {
+    if (Admin.token == req.body.token) {
+      return next();
+    } else {
+      return await res.status(401).json({
+        status: "error",
+        data: [],
+        msg: "Unauthorised",
+      });
     }
-    return await res.status(401).json({
-        status:"error",
-        data:[],
-        msg:"Unauthorised"
-    });
-    
-}
+  }
+  return await res.status(401).json({
+    status: "error",
+    data: [],
+    msg: "Unauthorised",
+  });
+};
 
 export { middleware };
