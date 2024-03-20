b6a56b59b841673719ac623b37f16eb15af85b91
kvineet002 <134228421+kvineet002@users.noreply.github.com>
2024-03-20 19:45:43 +0530

    Refactor emailService.js to use HTML template for OTP verification

--- Backend/services/emailService.js
+++ Backend/services/emailService.js
@@ -14,13 +14,48 @@ const emailService = {
         pass: PASSWORD, // Your Outlook password
       },
     });
-  
     let mailOptions = {
       from: EMAIL,
       to: `${email}@iitg.ac.in`,
       subject: 'OTP Verification For POR Portal',
-      text: `Your OTP for verification is: ${otp}`,
+      html: `
+      <html>
+      <head>
+        <style>
+          body {
+            font-family: Arial, sans-serif;
+            background-color: #212121;
+            color: #ffffff;
+          }
+          .container {
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 20px;
+            border: 1px solid #555;
+            background-color: #000;
+              text-align: center;
+          }
+          h2 {
+            color: #fff;
+          }
+          .otp-code {
+            font-size: 24px;
+            font-weight: bold;
+            color: #2164E8;
+          }
+ 
+        </style>
+      </head>
+      <body>
+        <div class="container">
+          <h2>OTP Verification</h2>
+          <p class='otp-line'>Your OTP for verification is: <span class="otp-code">${otp}</span></p>
+        </div>
+      </body>
+    </html>
+      `
     };
+    
   
     await transporter.sendMail(mailOptions);
   }
