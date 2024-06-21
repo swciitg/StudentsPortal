4392a73fb7c1eef88802e969d41c66251e01b4b5
ArushiKumar <arushiaro@gmail.com>
2024-06-21 12:00:13 +0530

    Recipients error solved

--- Frontend/src/Constants/recipients.js
+++ Frontend/src/Constants/recipients.js
@@ -1,10 +1,10 @@
  const recipients = [
-    { value: "m.geetanjay@iitg.ac.in", por: "m.geetanjay@iitg.ac.in" },
-    { value: "vineet.mech22@iitg.ac.in", por: "vineet.mech22@iitg.ac.in" },
-    { value: "k.dishant@iitg.ac.in", por: "k.dishant@iitg.ac.in" },
-    { value: "aryan.arya@iitg.ac.in", por: "aryan.arya@iitg.ac.in" },
-    { value: "Three", por: "Three" },
-    { value: "Four", por: "Four" },
+    { email: "m.geetanjay@iitg.ac.in", por: "m.geetanjay@iitg.ac.in" },
+    { email: "vineet.mech22@iitg.ac.in", por: "vineet.mech22@iitg.ac.in" },
+    { email: "k.dishant@iitg.ac.in", por: "k.dishant@iitg.ac.in" },
+    { email: "aryan.arya@iitg.ac.in", por: "aryan.arya@iitg.ac.in" },
+    { email: "Three", por: "Three" },
+    { email: "Four", por: "Four" },
   ];
 
 export default recipients;
